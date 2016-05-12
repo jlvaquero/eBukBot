1584be56386f374e140814898860ea3f51aa26a1
Jose Luis Vaquero Cuevas <jlvaquerocuevas@gmail.com>
2016-05-12 12:12:57 +0200

    no message

--- eBookBot.js
+++ eBookBot.js
@@ -142,7 +142,7 @@ bot.onText(/\Deutsch/, function(msg, match) {
  });
  redis.get(fromId + ':query').then((res) => {
   backInline_keyboard.inline_keyboard[0][0].switch_inline_query = res;
-  bot.sendMessage(fromId, 'Tap this' + '\u{1F447}' + 'button.', {
+  bot.sendMessage(fromId, 'Tap this button', {
    reply_markup: backInline_keyboard
   });
  });
